7f788d18617cf091feb8a72cf409b50fd5a4c252
Pranav Patel <Pranav.Patel@trigyn.com>
2021-07-21 20:15:16 +0530

    Code push

---
+++ Upload_Large_File_MVC_Core/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace Upload_Large_File_MVC_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment Environment;
+
+        public HomeController(IHostingEnvironment _environment)
+        {
+            Environment = _environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [RequestFormLimits(MultipartBodyLengthLimit = 2147483648)]
+        public IActionResult Index(List<IFormFile> postedFiles)
+        {
+            string wwwPath = this.Environment.WebRootPath;
+            string contentPath = this.Environment.ContentRootPath;
+
+            string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (IFormFile postedFile in postedFiles)
+            {
+                string fileName = Path.GetFileName(postedFile.FileName);
+                using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+                {
+                    ViewBag.Message += string.Format("<b>{0}</b> uploaded.<br />", fileName);
+                    postedFile.CopyTo(stream);
+                }
+            }
+
+            return View();
+        }
+    }
+}
